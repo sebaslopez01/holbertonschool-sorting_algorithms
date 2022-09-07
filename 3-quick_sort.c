d4feb55e16e3cedc1e0945f9d2a073e96b76be93
Sebastian <sebaslopez.2002@gmail.com>
2022-09-07 14:40:32 -0700

    3

---
+++ 3-quick_sort.c
@@ -0,0 +1,55 @@
+#include "sort.h"
+
+void swap(int *a, int *b)
+{
+	int temp_val = *a;
+	*a = *b;
+	*b = temp_val;
+}
+
+
+size_t partition(int *array, int low, int high)
+{
+	int i, pivot = array[high], greater_val = low - 1;
+
+	for (i = low; i < high; i++)
+	{
+		if (array[i] <= pivot)
+		{
+			greater_val++;
+			swap(&array[greater_val], &array[i]);
+		}
+	}
+
+	swap(&array[greater_val + 1], &array[high]);
+
+	return (greater_val + 1);
+}
+
+
+void quick_sort_helper(int *array, int low, int high)
+{
+	int pivot;
+
+	if (low < high)
+	{
+		pivot = partition(array, low, high);
+
+		quick_sort_helper(array, low, pivot + 1);
+
+		quick_sort_helper(array, pivot + 1, high);
+	}
+}
+
+/**
+ * quick_sort - Sorts an array of integers in ascending order
+ * using the QuickSort algorithm
+ * @array: Array to sort
+ * @size: Size of the array
+ *
+ * Return: void
+ */
+void quick_sort(int *array, size_t size)
+{
+	quick_sort_helper(array, 0, size - 1);
+}

1399d4bbf777c9ed327fdcfe9596aa5369951325
Sebastian <sebaslopez.2002@gmail.com>
2022-09-07 13:51:50 -0700

    2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -18,7 +18,7 @@ void selection_sort(int *array, size_t size)
 	{
 		min_idx = i;
 
-		for (j = i + 1; j < i; j++)
+		for (j = i + 1; j < size; j++)
 		{
 			if (array[j] < array[min_idx])
 				min_idx = j;

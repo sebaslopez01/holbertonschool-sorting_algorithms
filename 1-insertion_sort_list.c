1427378b85bfb875af0542e5b1eeb26b8057e523
Sebastian <sebaslopez.2002@gmail.com>
2022-09-06 19:35:55 -0700

    1

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -14,39 +14,40 @@ void insertion_sort_list(listint_t **list)
 
 	if (list == NULL || *list == NULL || (*list)->next == NULL)
 		return;
-
 	current = *list;
-
-	while (current->next != NULL)
+	while (current != NULL)
 	{
-		if (current->n > current->next->n)
+		if (current->next != NULL && current->n > current->next->n)
 		{
 			next_node = current->next;
-			
 			if (next_node->next != NULL)
 				next_node->next->prev = current;
-
 			current->next = current->next->next;
+			if (current->prev != NULL)
+				current->prev->next = next_node;
 			next_node->next = current;
 			next_node->prev = current->prev;
 			current->prev = next_node;
-		}
-		
-		prev_node = current->prev;
-		while (prev_node->prev != NULL)
-		{
-			if (prev_node->n < prev_node->prev->n)
+
+			prev_node = current->prev;
+			while (prev_node != NULL && prev_node->prev != NULL)
 			{
-				next_node = prev_node->prev;
-				
-				prev_node->next->prev = next_node;
-				next_node->next = prev_node->next;
-				prev_node->next = prev_node->prev;
-				prev_node->prev = prev_node->prev->prev;
-				next_node->prev = prev_node;
+				if (prev_node->n < prev_node->prev->n)
+				{
+					next_node = prev_node->prev;
+					prev_node->next->prev = next_node;
+					next_node->next = prev_node->next;
+					prev_node->next = prev_node->prev;
+					prev_node->prev = prev_node->prev->prev;
+					if (next_node->prev != NULL)
+						next_node->prev->next = prev_node;
+					next_node->prev = prev_node;
+				}
+				else
+					prev_node = prev_node->prev;
 			}
 		}
-
-		current = current->next;
+		else
+			current = current->next;
 	}
 }
